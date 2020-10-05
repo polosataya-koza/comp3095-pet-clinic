40d7ee8747157630d05c5241dabb63469436a3f2
Evgeniya <polosataya.koza@gmail.com>
2020-10-05 14:58:51 -0400

    First commit

---
+++ src/main/java/ca/gbc/comp3095/gbccomp3095pet/GbcComp3095PetApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbccomp3095pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcComp3095PetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbcComp3095PetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/comp3095/gbccomp3095pet/GbcComp3095PetApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbccomp3095pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcComp3095PetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
