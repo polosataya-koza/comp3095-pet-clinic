84621601c74e43a320897e41c73cdb92fddb816e
Evgeniya <polosataya.koza@gmail.com>
2020-10-08 22:55:47 -0400

    Add Pet Clinic Index Page and Controller

---
+++ pet-clinic-web/src/main/java/gbccomp3095pet/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package gbccomp3095pet.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping ({"", "/", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
